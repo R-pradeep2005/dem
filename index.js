857329a30e4a5429a758b1bd7ec2ca9a3fd079d0
R-pradeep2005 <web.dev.roar@gmail.com>
2024-09-08 14:11:55 +0530

    sign in and login

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hi from server");
+const express=require('express');
+const app= express();
+app.set('view engine' ,'ejs');
+app.set('views','./ejs_screens');
+app.get('/',(req,res) => {
+ res.render('sign-in')
+
+})
+app.get('/sign-up',(req,res)=>{
+  res.render('sign-up')
+})
+
+app.listen(3000,"0.0.0.0")
