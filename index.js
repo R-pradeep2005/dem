904b0ca15e14a5aec8c2be036f7c1b8f4ce5cfca
R-pradeep2005 <web.dev.roar@gmail.com>
2024-09-08 16:02:44 +0530

    signup and sign-in

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+
+
 const express=require('express');
+const fs=require('fs')
 const app= express();
+
 app.set('view engine' ,'ejs');
-app.set('views','./ejs_screens');
+app.set('views','./ejs_screens');     
 app.get('/',(req,res) => {
  res.render('sign-in')
 
@@ -9,5 +13,6 @@ app.get('/',(req,res) => {
 app.get('/sign-up',(req,res)=>{
   res.render('sign-up')
 })
+
 
 app.listen(3000,"0.0.0.0")
