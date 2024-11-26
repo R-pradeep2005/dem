a7deff465ab3538ee664e709e11efc0a1c10daff
Pradeep <web.dev.roar@gmail.com>
2024-11-26 20:14:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
-const express=require('express');
-const fs=require('fs');
+const express = require('express');
+const fs = require('fs');
 const path = require('path')
-const app= express();
-app.use(express.static(path.join(__dirname,'public')))
+const app = express();
+
+// Middleware
+app.use(express.static(path.join(__dirname,'public'))) 
+
 app.set('view engine' ,'ejs');
-app.set('views','./ejs_screens');     
-app.get('/',(req,res) => {
+app.set('views','./ejs_screens');
+
+app.get('/', (req, res) => {
  res.render('sign-in')
 })
-app.get('/sign-up',(req,res)=> {
+
+app.get('/sign-up', (req,res) => {
   res.render('sign-up')
 })
-app.get('/home',(req,res)=>{
+ 
+app.get('/home', (req, res) => {
   res.render('main_page')
 })
-app.get('/story',(req,res)=>{
+
+app.get('/story', (req,res) => {
 res.render('story')
 })
+
 app.post('/home',(req,res)=>{
   res.render('main_page')
 })
